3040e3e4e350dcb2fab7ccf43674897defadf84f
Laurent de Vries <laurent-devries@outlook.com>
2015-05-29 09:07:17 +0200

    ASP.NET APPLICATION START

---
+++ ASP.NET ICT4EVENTS/ASP.NET ICT4EVENTS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP.NET_ICT4EVENTS.Global" Language="C#" %>
---
+++ ASP.NET ICT4EVENTS/ASP.NET ICT4EVENTS/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NET_ICT4EVENTS.Startup))]
+namespace ASP.NET_ICT4EVENTS
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
