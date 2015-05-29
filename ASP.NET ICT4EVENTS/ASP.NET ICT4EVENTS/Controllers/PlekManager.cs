2511b80c6780c082ff595807a90efd90c5c31664
Laurent de Vries <laurent-devries@outlook.com>
2015-05-29 14:40:10 +0200

    Plekmanager method toegvoegd

--- ASP.NET ICT4EVENTS/ASP.NET ICT4EVENTS/Controllers/PlekManager.cs
+++ ASP.NET ICT4EVENTS/ASP.NET ICT4EVENTS/Controllers/PlekManager.cs
@@ -7,9 +7,25 @@ namespace ASP.NET_ICT4EVENTS.Models
 {
     public class PlekManager
     {
+        List<Plek> plekList;
         public PlekManager()
         {
-            
+            plekList = new List<Plek>();
+        }
+
+        public List<Plek> getPlekken()
+        {
+            return plekList;
+        }
+
+        public bool AddPlek(Plek plek)
+        {
+            return true;
+        }
+
+        public bool DeletePlek(Plek plek)
+        {
+            return true;
         }
     }
 }
