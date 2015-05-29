b1d4bbe4a4d8982da00ee39ae4e18e2b23c848c7
Laurent de Vries <laurent-devries@outlook.com>
2015-05-29 13:30:37 +0200

    Verhuurmanager methods toegevoegd

--- ASP.NET ICT4EVENTS/ASP.NET ICT4EVENTS/Models/VerhuurManager.cs
+++ ASP.NET ICT4EVENTS/ASP.NET ICT4EVENTS/Models/VerhuurManager.cs
@@ -7,9 +7,25 @@ namespace ASP.NET_ICT4EVENTS.Models
 {
     public class VerhuurManager
     {
+        List<Verhuur> VerhuurList;
         public VerhuurManager()
         {
+            VerhuurList = new List<Verhuur>();
+        }
 
+        public List<Verhuur> GetVerhuren()
+        {
+            return VerhuurList;
+        }
+
+        public bool AddVerhuur(Verhuur verhuur)
+        {
+            return true;
+        }
+
+        public bool DeleteVerhuur(Verhuur verhuur)
+        {
+            return true;
         }
     }
 }
