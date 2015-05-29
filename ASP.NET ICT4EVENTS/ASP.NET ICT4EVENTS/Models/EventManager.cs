50f2a7b79b6eb27400ec65d8c9e64061ac6b628b
Laurent de Vries <laurent-devries@outlook.com>
2015-05-29 13:41:03 +0200

    Eventmanager en Reserveringmanager methods toegevoegd

--- ASP.NET ICT4EVENTS/ASP.NET ICT4EVENTS/Models/EventManager.cs
+++ ASP.NET ICT4EVENTS/ASP.NET ICT4EVENTS/Models/EventManager.cs
@@ -7,9 +7,25 @@ namespace ASP.NET_ICT4EVENTS.Models
 {
     public class EventManager
     {
+        List<Event> eventList;
         public EventManager()
         {
+            eventList = new List<Event>();
+        }
 
+        public List<Event> GetEvents()
+        {
+            return eventList;
+        }
+
+        public bool addEvent(Event evenement)
+        {
+            return true;
+        }
+
+        public bool deleteEvent(Event evenement)
+        {
+            return true;
         }
     }
 }
---
+++ ASP.NET ICT4EVENTS/ASP.NET ICT4EVENTS/Models/ReserveringManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_ICT4EVENTS.Models
+{
+    public class ReserveringManager
+    {
+        List<Reservering> reserveringList;
+        public ReserveringManager()
+        {
+            reserveringList = new List<Reservering>();
+        }
+
+        public List<Reservering> GetReserveringen()
+        {
+            return reserveringList;
+        }
+
+        public bool AddReservering(Reservering reservering)
+        {
+            return true;
+        }
+
+        public bool DeleteReservering(Reservering reservering)
+        {
+            return true;
+        }
+    }
+}
