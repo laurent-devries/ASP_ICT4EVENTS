8178b1153d0d19a2acdb1eeb54984545f3936525
Laurent de Vries <laurent-devries@outlook.com>
2015-05-29 13:24:22 +0200

    ProductManager methods toegevoegd

--- ASP.NET ICT4EVENTS/ASP.NET ICT4EVENTS/Models/Bericht.cs
+++ ASP.NET ICT4EVENTS/ASP.NET ICT4EVENTS/Models/Bericht.cs
@@ -8,11 +8,13 @@ namespace ASP.NET_ICT4EVENTS.Models
     public class Bericht : Bijdrage
     {
         public string Inhoud { get; set; }
-
+        
         public Bericht(int id_Bijdrage, DateTime datum, string naam, string inhoud)
             : base(id_Bijdrage, datum, naam)
         {
             Inhoud = inhoud;
         }
     }
+
+    
 }
--- ASP.NET ICT4EVENTS/ASP.NET ICT4EVENTS/Models/ProductManager.cs
+++ ASP.NET ICT4EVENTS/ASP.NET ICT4EVENTS/Models/ProductManager.cs
@@ -7,9 +7,25 @@ namespace ASP.NET_ICT4EVENTS.Models
 {
     public class ProductManager
     {
+        List<Product> productList;
         public ProductManager()
         {
+            productList = new List<Product>();
+        }
 
+        public bool AddProduct(Product product)
+        {
+            return true;
+        }
+
+        public bool DeleteProduct(Product product)
+        {
+            return true;
+        }
+
+        public List<Product> GetProducts()
+        {
+            return productList;
         }
     }
 }
